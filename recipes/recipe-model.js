6c2a659b5790c2cb8cc54ffe09b27f7e36f9e6cc
Ryan Ansberry <ansman92@gmail.com>
2019-08-29 17:04:26 -0700

    Initial Project Layout

---
+++ recipes/recipe-model.js
@@ -0,0 +1,29 @@
+const db = require('../data/db-config.js');
+
+module.exports = {
+    find,
+    findById,
+    add,
+    update,
+    remove
+};
+
+function find() {
+
+}
+  
+function findById(id) {
+
+}
+
+function add(recipe) {
+
+}
+
+function update(changes, id) {
+
+}
+
+function remove(id) {
+
+}
---
+++ recipes/recipe-router.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const Recipes = require('./recipe-model.js');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+
+router.post('/', (req, res) => {
+
+});
+
+router.put('/:id', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
+
+module.exports = router;
